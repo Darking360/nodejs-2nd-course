35d1c99ce78e863b09265c5c4ae4da5b260956ba
Miguel Bolivar <MBolivar360@gmail.com>
2018-10-06 23:57:07 -0400

    Refactoring geocode into its own file and importing it in app.js and reusing it correctly.-

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,6 @@
 const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode');
 
 const args = yargs
   .options({
@@ -11,15 +12,10 @@ const args = yargs
   })
   .argv;
 
-const buildGeocodeURL = (address) => {
-  return `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=AIzaSyA40ocB_Ddr7l94XvnaXaugpKp02nhvDF0`
-};
-
-request({
-  url: buildGeocodeURL(args.address),
-  json: true
-}, (error, respponse, body) => {
-  console.log(`Address: ${body.results[0].formatted_address}`);
-  console.log(`Latitude: body.results[0].geometry.location.lat`);
-  console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
-});
+geocode.getCoords(args.address, (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+})
